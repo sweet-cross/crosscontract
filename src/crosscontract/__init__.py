724dbd1c8165a293368cf0939283e607c1f605ad
semantic-release <semantic-release>
2026-02-12 19:24:43 +0000

    1.0.0
    Automatically generated by python-semantic-release

--- src/crosscontract/__init__.py
+++ src/crosscontract/__init__.py
@@ -3,7 +3,7 @@
 from .contracts import BaseContract, CrossContract, SchemaValidationError, TableSchema
 from .crossclient import CrossClient
 
-__version__ = "0.1.0"
+__version__ = "1.0.0"
 
 __all__ = [
     "CrossClient",
