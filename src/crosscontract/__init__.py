80a10db9f742ce3594f060b9465d72f3dac08a84
jabrell <4337825+jabrell@users.noreply.github.com>
2026-06-05 17:41:41 +0200

    Fix: parquet for upload (#62)
    Co-authored-by: semantic-release <semantic-release>

--- src/crosscontract/__init__.py
+++ src/crosscontract/__init__.py
@@ -4,7 +4,7 @@ from .contracts import BaseContract, CrossContract, SchemaValidationError, Table
 from .crossclient import CrossClient
 from .registry import CrossRegistry
 
-__version__ = "0.10.0"
+__version__ = "0.10.1"
 
 __all__ = [
     "CrossClient",
