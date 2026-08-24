6531c8dc1ee5784d429f16bb42ab9524f2dbcd25
semantic-release <semantic-release>
2026-08-24 11:25:54 +0000

    0.16.1
    [skip release]

--- src/crosscontract/__init__.py
+++ src/crosscontract/__init__.py
@@ -5,7 +5,7 @@ from .crossclient import CrossClient
 from .registry import CrossRegistry
 from .submission import SubmissionContract
 
-__version__ = "0.16.0"
+__version__ = "0.16.1"
 
 __all__ = [
     "CrossClient",
